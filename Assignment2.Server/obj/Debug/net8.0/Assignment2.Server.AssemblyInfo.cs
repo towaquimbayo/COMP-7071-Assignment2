6a0f55b13cda6928a7827a23b193226a0a915ad4
Towa Quimbayo <towacurtis@hotmail.com>
2025-02-12 23:19:49 -0800

    ud csproj

--- Assignment2.Server/obj/Debug/net8.0/Assignment2.Server.AssemblyInfo.cs
+++ Assignment2.Server/obj/Debug/net8.0/Assignment2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91a4f51685d76c4507333b00ebd288b92da7171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cad0b4519833df70ab6c1d30f5acc1dd6ba40c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
