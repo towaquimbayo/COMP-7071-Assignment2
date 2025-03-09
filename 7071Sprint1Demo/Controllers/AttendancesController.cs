95f3979eb46fcccc4955f19cdf74e19dc2be41fc
MaximillianYong <syong14@my.bcit.ca>
2025-03-09 00:43:39 -0800

    Create Attendance controller and views

---
+++ 7071Sprint1Demo/Controllers/AttendancesController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _7071Sprint1Demo.Data;
+using _7071Sprint1Demo.Models;
+
+namespace _7071Sprint1Demo.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Attendances
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Attendances.Include(a => a.Employee).Include(a => a.Shift);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Attendances/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .Include(a => a.Employee)
+                .Include(a => a.Shift)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // GET: Attendances/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Address");
+            ViewData["ShiftId"] = new SelectList(_context.Shifts, "Id", "Id");
+            return View();
+        }
+
+        // POST: Attendances/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EmployeeId,ShiftId,ApprovedByManager,Notes")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                attendance.Id = Guid.NewGuid();
+                _context.Add(attendance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Address", attendance.EmployeeId);
+            ViewData["ShiftId"] = new SelectList(_context.Shifts, "Id", "Id", attendance.ShiftId);
+            return View(attendance);
+        }
+
+        // GET: Attendances/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Address", attendance.EmployeeId);
+            ViewData["ShiftId"] = new SelectList(_context.Shifts, "Id", "Id", attendance.ShiftId);
+            return View(attendance);
+        }
+
+        // POST: Attendances/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,EmployeeId,ShiftId,ApprovedByManager,Notes")] Attendance attendance)
+        {
+            if (id != attendance.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(attendance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AttendanceExists(attendance.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Address", attendance.EmployeeId);
+            ViewData["ShiftId"] = new SelectList(_context.Shifts, "Id", "Id", attendance.ShiftId);
+            return View(attendance);
+        }
+
+        // GET: Attendances/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.Attendances
+                .Include(a => a.Employee)
+                .Include(a => a.Shift)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        // POST: Attendances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance != null)
+            {
+                _context.Attendances.Remove(attendance);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AttendanceExists(Guid id)
+        {
+            return _context.Attendances.Any(e => e.Id == id);
+        }
+    }
+}
